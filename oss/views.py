772c4c5f29908173a534f819c78233fccd5d3124
jiazhichao <wit235813@gmail.com>
2019-04-27 20:44:27 +0800

    plup1

--- oss/views.py
+++ oss/views.py
@@ -8,6 +8,7 @@ import datetime
 import json
 import hmac
 from hashlib import sha1 as sha
+from music.models import Song
 
 # 请填写您的AccessKeyId。
 access_key_id = 'LTAI5AYKfHBArSBR'
@@ -18,7 +19,7 @@ host = 'http://pandacoderblog.oss-cn-shanghai.aliyuncs.com';
 # callback_url为 上传回调服务器的URL，请将下面的IP和Port配置为您自己的真实信息。
 callback_url = "http://pandacoder.top/oss/";
 # 用户上传文件时指定的前缀。
-upload_dir = 'calltest/'
+upload_dir = 'music/'
 expire_time = 30
 
 # Create your views here.
@@ -50,7 +51,7 @@ def get_token():
     album=3
     callback_dict = {}
     callback_dict['callbackUrl'] = callback_url;
-    callback_dict['callbackBody'] = 'filename=${object}&size=${size}&aa={x:aa}';
+    callback_dict['callbackBody'] = 'filename=${object}&size=${size}';
     callback_dict['callbackBodyType'] = 'application/x-www-form-urlencoded';
     callback_param = json.dumps(callback_dict).strip()
     print(callback_param)
@@ -75,55 +76,10 @@ def tem(request):
         print('88880000'+str(request.POST))
         return HttpResponse('{"Status":"OK"}'.encode())
 
-def do_POST(server):
-    """
-    启用 POST 调用处理逻辑
-    :param server: Web HTTP Server 服务
-    :return:
-    """
-    print("********************* do_POST ")
-    # get public key
-    pub_key_url = ''
-
-    try:
-        pub_key_url_base64 = server.headers['x-oss-pub-key-url']
-        pub_key = httpserver.get_pub_key(pub_key_url_base64)
-    except Exception as e:
-        print(str(e))
-        print('Get pub key failed! pub_key_url : ' + pub_key_url)
-        server.send_response(400)
-        server.end_headers()
-        return
-
-    # get authorization
-    authorization_base64 = server.headers['authorization']
-
-    # get callback body
-    content_length = server.headers['content-length']
-    callback_body = server.rfile.read(int(content_length))
-
-    # compose authorization string
-    auth_str = ''
-    pos = server.path.find('?')
-    if -1 == pos:
-        auth_str = server.path + '\n' + callback_body.decode()
-    else:
-        auth_str = httpserver.get_http_request_unquote(server.path[0:pos]) + server.path[pos:] + '\n' + callback_body
-
-    result = httpserver.verrify(auth_str, authorization_base64, pub_key)
-
-    if not result:
-        print('Authorization verify failed!')
-        print('Public key : %s' % (pub_key))
-        print('Auth string : %s' % (auth_str))
-        server.send_response(400)
-        server.end_headers()
-        return
-
-    # response to OSS
-    resp_body = '{"Status":"OK"}'
-    server.send_response(200)
-    server.send_header('Content-Type', 'application/json')
-    server.send_header('Content-Length', str(len(resp_body)))
-    server.end_headers()
-    server.wfile.write(resp_body.encode())
+def do_POST(request):
+    Song.album=request.POST['albumid'][0]
+    Song.song_title=request.POST['filename'][0]
+    Song.audio_file='https://pandacoderblog.oss-cn-shanghai.aliyuncs.com/'+request.POST['filename'][0]
+    Song.ytburl=request.POST['ytb'][0]
+    Song.length=201
+    Song.save()
