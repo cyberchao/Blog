dd129304ba4e8ca245d778f8b50154625ad750b2
jiazhichao <wit235813@gmail.com>
2019-04-27 22:31:14 +0800

    mp3

--- static_root/js/upload.js
+++ static_root/js/upload.js
@@ -161,8 +161,7 @@ var uploader = new plupload.Uploader({
 
     filters: {
         mime_types : [ //只允许上传图片和zip文件
-        { title : "Image files", extensions : "mp3" },
-        { title : "Zip files", extensions : "zip,rar" }
+        { title : "Image files", extensions : "mp3, ogg, wav, wma" },
         ],
         max_file_size : '10mb', //最大只能上传10mb的文件
         prevent_duplicates : true //不允许选取重复文件
