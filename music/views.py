5a6e4b7c4084e39e4a63cb15590d1955e2f5d1e9
jiazhichao <1328155746@qq.com>
2019-04-29 13:51:16 +0800

    add cloud torrent

--- music/views.py
+++ music/views.py
@@ -14,7 +14,8 @@ def index(request):
     if not request.user.is_authenticated:
         return render(request, 'login.html')
     else:
-        albums = Album.objects.filter(user=request.user)
+        # albums = Album.objects.filter(user=request.user)
+        albums = Album.objects.all()
         song_results = Song.objects.all()
         query = request.GET.get("q")
         if query:
